a6bd21e96a32c5dfebb4d51f64eef9047e6f0282
Parth <ippman@localhost.localdomain>
2025-08-30 14:08:37 +0530

    Final commit.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,16 @@
 
 using namespace std;
 
+Color Green = Color{38, 185, 154, 255};
+Color Dark_Green = Color{20, 160, 133, 255};
+Color Light_Green = Color{129, 204, 184, 255};
+Color Yellow = Color{243, 213, 91, 255};
+
+
+
+int player_score = 0;
+int cpu_score = 0;
+
 class Ball{
     public:
     float x, y;
@@ -10,7 +20,7 @@ class Ball{
     int radius;
 
     void Draw(){
-        DrawCircle(x, y, radius, WHITE);
+        DrawCircle(x, y, radius, Yellow);
 
     }
 
@@ -21,25 +31,94 @@ class Ball{
         if(y + radius >= GetScreenHeight() || y - radius <= 0){
             speed_y *= -1;
         }
-        if(x + radius >= GetScreenWidth() || x - radius <= 0){
-            speed_x *= -1;
+        if(x + radius >= GetScreenWidth()) //cpu wins
+        {
+            cpu_score++;
+            ResetBall();
+        }
+        
+        if(x - radius <= 0)
+        {
+            player_score++;
+            ResetBall();
         }
     }
+
+
+    void ResetBall()
+    {
+        x = GetScreenWidth()/2;
+        y = GetScreenHeight()/2;
+
+        int speed_choices[2] = {-1, 1};
+        speed_x *= speed_choices[GetRandomValue(0,1)];
+        speed_y *= speed_choices[GetRandomValue(0,1)];
+
+    }
+
 };
 
 class Paddle{
+
+    protected:
+    void LimitMovement()
+    {
+        if(y <= 0)
+        {
+            y = 0;
+        }
+        if(y+ height >= GetScreenHeight())
+        {
+            y = GetScreenHeight() - height;
+        }
+    }
+
+
     public:
     float x, y;
     float width, height;
     int speed;
 
-    void Draw(){
-        DrawRectangle(x, y, width, height, WHITE);
+    void Draw()
+    {
+        DrawRectangleRounded(Rectangle{x, y, width, height}, 0.8, 0, WHITE);
+    }
 
+    void Update(){
+        if(IsKeyDown(KEY_UP))
+        {
+            y = y - speed;
+        }
+        if(IsKeyDown(KEY_DOWN))
+        {
+            y = y + speed;
+        }
+
+        LimitMovement();
+    }   
+};
+
+class CpuPaddle: public Paddle{
+    public:
+
+    void Update(int ball_y)
+    {
+        if(y + height/2 > ball_y)
+        {
+            y = y - speed;
+        }
+        if(y + height/2 <= ball_y)
+        {
+            y = y + speed;
+        }
+        LimitMovement();
     }
 };
 
+
 Ball ball;
+Paddle player;
+CpuPaddle cpu;
 
 int main () {
 
@@ -55,18 +134,50 @@ int main () {
     ball.speed_x = 7;
     ball.speed_y = 7;
 
+    player.width = 25;
+    player.height = 120;
+    player.x = screen_width - player.width-10;
+    player.y = screen_height/2 - player.height/2;
+    player.speed = 6;
+
+    cpu.height = 120;
+    cpu.width = 25;
+    cpu.x = 10;
+    cpu.y = screen_height/2 - cpu.height/2;
+    cpu.speed = 6;
+
     
     while(WindowShouldClose() == false){
         BeginDrawing();
 
         //Updating
         ball.Update();
+        player.Update();
+        cpu.Update(ball.y);
+
+
+        //Checking for collisions
+        if(CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player.x, player.y, player.width, player.height}))
+        {
+            ball.speed_x *= -1;
+        }
+
+        if(CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{cpu.x, cpu.y, cpu.width, cpu.height}))
+        {
+            ball.speed_x *= -1;
+        }
 
         //Drawing
-        ClearBackground(BLACK);
+        ClearBackground(Dark_Green);
+        DrawRectangle(screen_width/2, 0, screen_width/2, screen_height, Green);
+        DrawCircle(screen_width/2, screen_height/2, 150, Light_Green);
         DrawLine(screen_width/2, 0, screen_width/2, screen_height, WHITE);
         ball.Draw();
-        DrawRectangle(screen_width-35, screen_height/2-60, 25, 120, WHITE);
+        cpu.Draw();
+        player.Draw();
+        DrawText(TextFormat("%i", cpu_score), screen_width/4-20, 20, 80, WHITE);
+        DrawText(TextFormat("%i", player_score), 3 * screen_width/4-20, 20, 80, WHITE);
+
 
         EndDrawing();
 
